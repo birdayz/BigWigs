a976b272fca57141328b0a825114ee1a444f277f
Johannes Brüderl <birdy@nerden.de>
2017-05-27 23:06:11 +0200

    Adjust Magmadar, Broodlord (#9)
    * update magmadar timers
    
    * update broodlord timers
    
    * fix vael formatting

--- Raids/BWL/Broodlord.lua
+++ Raids/BWL/Broodlord.lua
@@ -10,55 +10,58 @@ local L = AceLibrary("AceLocale-2.2"):new("BigWigs"..boss)
 ----------------------------
 
 L:RegisterTranslations("enUS", function() return {
-	cmd = "Broodlord",
+  cmd = "Broodlord",
 
-	engage_trigger = "None of your kind should be here",
-	ms_trigger = "^(.+) (.+) afflicted by Mortal Strike",
-	bw_trigger = "^(.+) (.+) afflicted by Blast Wave",
-	deathyou_trigger = "You die\.",
-	deathother_trigger = "(.+) dies\.",
-	ms_warn_you = "Mortal Strike on you!",
-	ms_warn_other = "Mortal Strike on %s!",
-	bw_warn = "Blast Wave soon!",
-	ms_bar = "Mortal Strike: %s",
-	bw_bar = "Blast Wave",
+  engage_trigger = "None of your kind should be here",
+  ms_trigger = "^(.+) (.+) afflicted by Mortal Strike",
+  bw_trigger = "^(.+) (.+) afflicted by Blast Wave",
+  knock_away_trigger1 = "s Knock Away hits",
+  deathyou_trigger = "You die\.",
+  deathother_trigger = "(.+) dies\.",
+  ms_warn_you = "Mortal Strike on you!",
+  ms_warn_other = "Mortal Strike on %s!",
+  bw_warn = "Blast Wave soon!",
+  ms_bar = "Mortal Strike: %s",
+  bw_bar = "Possible Blast Wave",
+  bw_bar_first = "First Blast Wave",
+  knock_away_possible_bar = "Possible Knock Back",
 
-	you = "You",
-	are = "are",
+  you = "You",
+  are = "are",
 
-	ms_cmd = "ms",
-	ms_name = "Mortal Strike",
-	ms_desc = "Warn when someone gets Mortal Strike and starts a clickable bar for easy selection.",
+  ms_cmd = "ms",
+  ms_name = "Mortal Strike",
+  ms_desc = "Warn when someone gets Mortal Strike and starts a clickable bar for easy selection.",
 
-	bw_cmd = "bw",
-	bw_name = "Blast Wave",
-	bw_desc = "Shows a bar with the possible Blast Wave cooldown.\n\n(Disclaimer: this varies anywhere from 8 to 15 seconds. Chosen shortest interval for safety.)",
+  bw_cmd = "bw",
+  bw_name = "Blast Wave",
+  bw_desc = "Shows a bar with the possible Blast Wave cooldown.\n\n(Disclaimer: this varies anywhere from 8 to 15 seconds. Chosen shortest interval for safety.)",
 } end )
 
 L:RegisterTranslations("deDE", function() return {
-	cmd = "Broodlord",
-	
-	engage_trigger = "None of your kind should be here",
-	ms_trigger = "^(.+) (.+) von T\195\182dlicher Sto\195\159 betroffen",
-	bw_trigger = "^(.+) (.+) von Druckwelle betroffen",
-	deathyou_trigger = "Du stirbst\.",
-	deathother_trigger = "(.+) stirbt\.",
-	ms_warn_you = "T\195\182dlicher Sto\195\159 auf Dir!",
-	ms_warn_other = "T\195\182dlicher Sto\195\159 auf %s!",
-	bw_warn = "Druckwelle bald!",
-	ms_bar = "T\195\182dlicher Sto\195\159: %s",
-	bw_bar = "Druckwelle",
+  cmd = "Broodlord",
 
-	you = "Ihr",
-	are = "seid",
+  engage_trigger = "None of your kind should be here",
+  ms_trigger = "^(.+) (.+) von T\195\182dlicher Sto\195\159 betroffen",
+  bw_trigger = "^(.+) (.+) von Druckwelle betroffen",
+  deathyou_trigger = "Du stirbst\.",
+  deathother_trigger = "(.+) stirbt\.",
+  ms_warn_you = "T\195\182dlicher Sto\195\159 auf Dir!",
+  ms_warn_other = "T\195\182dlicher Sto\195\159 auf %s!",
+  bw_warn = "Druckwelle bald!",
+  ms_bar = "T\195\182dlicher Sto\195\159: %s",
+  bw_bar = "Druckwelle",
 
-	ms_cmd = "ms",
-	ms_name = "T\195\182dlicher Sto\195\159",
-	ms_desc = "Warnung, wenn Spieler von T\195\182dlicher Sto\195\159 betroffen sind und beginnt einen anklickbaren Balken f\195\188r einfache Auswahl.",
-	
-	bw_cmd = "bw",
-	bw_name = "Druckwelle",
-	bw_desc = "Zeigt eine Balken mit der m\195\182glichen Druckwelle Abklingzeit.\n\n(Dementi: Diese variiert \195\188berall von 8 bis 15 den Sekunden Sie wurde k\195\188rzeste Intervall für die Sicherheit entschieden.)",
+  you = "Ihr",
+  are = "seid",
+
+  ms_cmd = "ms",
+  ms_name = "T\195\182dlicher Sto\195\159",
+  ms_desc = "Warnung, wenn Spieler von T\195\182dlicher Sto\195\159 betroffen sind und beginnt einen anklickbaren Balken f\195\188r einfache Auswahl.",
+
+  bw_cmd = "bw",
+  bw_name = "Druckwelle",
+  bw_desc = "Zeigt eine Balken mit der m\195\182glichen Druckwelle Abklingzeit.\n\n(Dementi: Diese variiert \195\188berall von 8 bis 15 den Sekunden Sie wurde k\195\188rzeste Intervall für die Sicherheit entschieden.)",
 } end )
 
 ----------------------------------
@@ -78,16 +81,18 @@ BigWigsBroodlord:RegisterYellEngage(L["engage_trigger"])
 ------------------------------
 
 function BigWigsBroodlord:OnEnable()
-    self.started = nil
-	self.lastbw = 0
-    self.lastMS = 0
-    self.MS = ""
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
-	self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
-    self:RegisterEvent("PLAYER_TARGET_CHANGED")
-	self:RegisterEvent("CHAT_MSG_COMBAT_FRIENDLY_DEATH")
+  self.started = nil
+  self.lastbw = 0
+  self.lastMS = 0
+  self.MS = ""
+  self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Event")
+  self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Event")
+  self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Event")
+  self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Event")
+  self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Event")
+  self:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+  self:RegisterEvent("PLAYER_TARGET_CHANGED")
+  self:RegisterEvent("CHAT_MSG_COMBAT_FRIENDLY_DEATH")
 end
 
 ------------------------------
@@ -95,53 +100,64 @@ end
 ------------------------------
 
 function BigWigsBroodlord:Event(msg)
-	local _, _, name, detect = string.find(msg, L["ms_trigger"])
-	if name and detect and self.db.profile.ms then
-        self.MS = name
-        self.lastMS = GetTime()
-		if detect == L["are"] then
-			self:TriggerEvent("BigWigs_Message", L["ms_warn_you"], "Urgent")
-			self:TriggerEvent("BigWigs_StartBar", self, string.format(L["ms_bar"], UnitName("player")), 5, "Interface\\Icons\\Ability_Warrior_SavageBlow", true, "Black")
-			self:SetCandyBarOnClick("BigWigsBar "..string.format(L["ms_bar"], UnitName("player")), function(name, button, extra) TargetByName(extra, true) end, UnitName("player"))
-            self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Ability_Warrior_SavageBlow", 5)
-		else
-			self:TriggerEvent("BigWigs_Message", string.format(L["ms_warn_other"], name), "Attention")
-			self:TriggerEvent("BigWigs_StartBar", self, string.format(L["ms_bar"], name), 5, "Interface\\Icons\\Ability_Warrior_SavageBlow", true, "Black")
-			self:SetCandyBarOnClick("BigWigsBar "..string.format(L["ms_bar"], name), function(name, button, extra) TargetByName(extra, true) end, name)
-		end
-	elseif string.find(msg, L["bw_trigger"]) and self.db.profile.bw then
-		if GetTime() - self.lastbw > 5 then
-			self:TriggerEvent("BigWigs_StartBar", self, L["bw_bar"], 8, "Interface\\Icons\\Spell_Holy_Excorcism_02", true, "Red")
-			self:ScheduleEvent("BigWigs_Message", 24, L["bw_warn"], "Urgent", true, "Alert")
-		end
-		self.lastbw = GetTime()
-	end
+  local _, _, name, detect = string.find(msg, L["ms_trigger"])
+  if name and detect and self.db.profile.ms then
+    self.MS = name
+    self.lastMS = GetTime()
+    if detect == L["are"] then
+      self:TriggerEvent("BigWigs_Message", L["ms_warn_you"], "Urgent")
+      self:TriggerEvent("BigWigs_StartBar", self, string.format(L["ms_bar"], UnitName("player")), 5, "Interface\\Icons\\Ability_Warrior_SavageBlow", true, "Black")
+      self:SetCandyBarOnClick("BigWigsBar "..string.format(L["ms_bar"], UnitName("player")), function(name, button, extra) TargetByName(extra, true) end, UnitName("player"))
+      self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Ability_Warrior_SavageBlow", 5)
+    else
+      self:TriggerEvent("BigWigs_Message", string.format(L["ms_warn_other"], name), "Attention")
+      self:TriggerEvent("BigWigs_StartBar", self, string.format(L["ms_bar"], name), 5, "Interface\\Icons\\Ability_Warrior_SavageBlow", true, "Black")
+      self:SetCandyBarOnClick("BigWigsBar "..string.format(L["ms_bar"], name), function(name, button, extra) TargetByName(extra, true) end, name)
+    end
+  elseif string.find(msg, L["bw_trigger"]) and self.db.profile.bw then
+    if GetTime() - self.lastbw > 5 then
+      self:TriggerEvent("BigWigs_StopBar", self, L["bw_bar"])
+      self:TriggerEvent("BigWigs_StartBar", self, L["bw_bar"], 20, "Interface\\Icons\\Spell_Holy_Excorcism_02", true, "Red")
+      self:ScheduleEvent("BigWigs_Message", 15, L["bw_warn"], "Urgent", true, "Alert")
+      self:SetCandyBarFade("BigWigsBar "..L["bw_bar"], 15)
+    end
+    self.lastbw = GetTime()
+  elseif string.find(msg, L["knock_away_trigger1"]) then
+    self:TriggerEvent("BigWigs_StopBar", self, L["knock_away_possible_bar"])
+    self:TriggerEvent("BigWigs_StartBar", self, L["knock_away_possible_bar"], 12, "Interface\\Icons\\INV_Gauntlets_05", true, "Red")
+    self:ScheduleEvent("BigWigs_Message", 7, L["knock_away_possible_bar"], "Urgent", true, "Alert")
+    self:SetCandyBarFade("BigWigsBar "..L["knock_away_possible_bar"], 13)
+  end
 end
 
 function BigWigsBroodlord:CHAT_MSG_COMBAT_FRIENDLY_DEATH(msg)
-	if not self.db.profile.bw then return end
-	local _, _, deathother = string.find(msg, L["deathother_trigger"])
-	if msg == L["deathyou_trigger"] then
-		self:TriggerEvent("BigWigs_StopBar", self, string.format(L["ms_bar"], UnitName("player")))
-	elseif deathother then
-		self:TriggerEvent("BigWigs_StopBar", self, string.format(L["ms_bar"], deathother))
-	end
+  if not self.db.profile.bw then return end
+  local _, _, deathother = string.find(msg, L["deathother_trigger"])
+  if msg == L["deathyou_trigger"] then
+    self:TriggerEvent("BigWigs_StopBar", self, string.format(L["ms_bar"], UnitName("player")))
+  elseif deathother then
+    self:TriggerEvent("BigWigs_StopBar", self, string.format(L["ms_bar"], deathother))
+  end
 end
 
 function BigWigsBroodlord:CHAT_MSG_MONSTER_YELL(msg)
-	if not self.db.profile.bw then return end
-	if string.find(msg, L["engage_trigger"]) and not self.started then
-		self:TriggerEvent("BigWigs_StartBar", self, L["bw_bar"], 19.5, "Interface\\Icons\\Spell_Holy_Excorcism_02", true, "Red")
-		self:ScheduleEvent("BigWigs_Message", 14.5, L["bw_warn"], "Urgent", true, "Alert")
-        self:SendEngageSync()
-        self:StartFight()
-	end
+  if not self.db.profile.bw then return end
+  if string.find(msg, L["engage_trigger"]) and not self.started then
+    self:TriggerEvent("BigWigs_StartBar", self, L["bw_bar_first"], 20, "Interface\\Icons\\Spell_Holy_Excorcism_02", true, "Red")
+    self:ScheduleEvent("BigWigs_Message", 15, L["bw_warn"], "Urgent", true, "Alert")
+    self:SendEngageSync()
+    self:StartFight()
+
+    self:TriggerEvent("BigWigs_StartBar", self, L["knock_away_possible_bar"], 20, "Interface\\Icons\\INV_Gauntlets_05", true, "Red")
+    self:ScheduleEvent("BigWigs_Message", 15, L["knock_away_possible_bar"], "Urgent", true, "Alert")
+    self:SetCandyBarFade("BigWigsBar "..L["knock_away_possible_bar"], 5)
+  end
 end
 
 function BigWigsBroodlord:PLAYER_TARGET_CHANGED()
-    if (self.lastMS + 5) > GetTime() and UnitName("target") == self.MS then
-        self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Ability_Warrior_SavageBlow", (self.lastMS + 5) - GetTime())
-    else
-        self:TriggerEvent("BigWigs_HideIcon", "Interface\\Icons\\Ability_Warrior_SavageBlow")
-    end
+  if (self.lastMS + 5) > GetTime() and UnitName("target") == self.MS then
+    self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Ability_Warrior_SavageBlow", (self.lastMS + 5) - GetTime())
+  else
+    self:TriggerEvent("BigWigs_HideIcon", "Interface\\Icons\\Ability_Warrior_SavageBlow")
+  end
 end
--- Raids/MC/Magmadar.lua
+++ Raids/MC/Magmadar.lua
@@ -11,52 +11,59 @@ local _, playerClass = UnitClass("player")
 ----------------------------
 
 L:RegisterTranslations("enUS", function() return {
-	-- Chat message triggers
-	trigger1 = "goes into a killing frenzy!",
-	trigger2 = "afflicted by Panic.",
-	trigger3 = "Panic fail(.+) immune.",
-	trigger4 = "Magmadar's Panic was resisted",
-	frenzy_bar = "Frenzy",
-	frenzyfade_trigger = "Frenzy fades from Magmadar",
+  -- Chat message triggers
+  trigger1 = "goes into a killing frenzy!",
+  trigger2 = "afflicted by Panic.",
+  trigger3 = "Panic fail(.+) immune.",
+  trigger4 = "Magmadar's Panic was resisted",
+  frenzy_active_bar = "Frenzy active",
+  frenzy_bar = "Possible Frenzy",
+  frenzy_first_bar = "Frenzy",
+  frenzyfade_trigger = "Frenzy fades from Magmadar",
 
-	-- Warnings and bar texts
-	frenzyann = "Frenzy! Tranq now!",
-	fearsoon = "Panic incoming soon!",
-	feartime = "Fear! ~30 seconds until next!",
-	fearbar = "Panic",
+  -- Warnings and bar texts
+  frenzyann = "Frenzy! Tranq now!",
+  fearsoon = "Panic incoming soon!",
+  feartime = "Fear! ~30 seconds until next!",
+  fear_first_bar = "Fear",
+  fearbar = "Possible Fear",
 
-	-- AceConsole strings
-	cmd = "Magmadar",
-	
-	panic_cmd = "panic",
-	panic_name = "Warn for Panic",
-	panic_desc = "Warn when Magmadar casts Panic",
-	
-	frenzy_cmd = "frenzy",
-	frenzy_name = "Frenzy alert",
-	frenzy_desc = "Warn when Magmadar goes into a frenzy",
+  -- AceConsole strings
+  cmd = "Magmadar",
+
+  panic_cmd = "panic",
+  panic_name = "Warn for Panic",
+  panic_desc = "Warn when Magmadar casts Panic",
+
+  frenzy_cmd = "frenzy",
+  frenzy_name = "Frenzy alert",
+  frenzy_desc = "Warn when Magmadar goes into a frenzy",
 } end)
 
 L:RegisterTranslations("deDE", function() return {
-	trigger1 = "goes into a killing frenzy!",
-	trigger2 = "von Panik betroffen",
-	trigger3 = "Panik(.+)immun",
-	trigger4 = "Panik(.+)widerstanden",
-	frenzy_bar = "Wutanfall",
-	frenzyfade_trigger = "Wutanfall schwindet von Magmadar.",
+  trigger1 = "goes into a killing frenzy!",
+  trigger2 = "von Panik betroffen",
+  trigger3 = "Panik(.+)immun",
+  trigger4 = "Panik(.+)widerstanden",
 
-	frenzyann = "Raserei! Tranq jetzt!",
-	fearsoon = "Panik in 5 Sekunden!",
-	feartime = "AoE Furcht! N\195\164chste in ~30 Sekunden!",
-	fearbar = "Panik",
+  frenzy_active_bar = "Magmadar ist wütend!",
+  frenzy_bar = "Möglicher Wutanfall",
+  frenzy_first_bar = "Wutanfall",
+  frenzyfade_trigger = "Wutanfall schwindet von Magmadar.",
 
-	panic_cmd = "panic",
-	panic_name = "Alarm f\195\188r Panik",
-	panic_desc = "Warnung, wenn Magmadar AoE Furcht wirkt.",
-	
-	frenzy_cmd = "frenzy",
-	frenzy_name = "Alarm f\195\188r Raserei",
-	frenzy_desc = "Warnung, wenn Magmadar in Raserei ger\195\164t.",
+  frenzyann = "Raserei! Jetzt einlullen!!11einself",
+  fearsoon = "Panik in 5 Sekunden!",
+  feartime = "AoE Furcht! N\195\164chste in ~30 Sekunden!",
+  fear_first_bar = "Fear",
+  fearbar = "Panik",
+
+  panic_cmd = "panic",
+  panic_name = "Alarm f\195\188r Panik",
+  panic_desc = "Warnung, wenn Magmadar AoE Furcht wirkt.",
+
+  frenzy_cmd = "frenzy",
+  frenzy_name = "Alarm f\195\188r Raserei",
+  frenzy_desc = "Warnung, wenn Magmadar in Raserei ger\195\164t.",
 } end)
 
 ----------------------------------
@@ -75,21 +82,20 @@ BigWigsMagmadar.revision = tonumber(string.sub("$Revision: 11204 $", 12, -3))
 ------------------------------
 
 function BigWigsMagmadar:OnEnable()
-	firstpanic = 0
-	self:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
-	self:RegisterEvent("CHAT_MSG_SPELL_AURA_GONE_OTHER")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Panic")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Panic")
-	self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Panic")
-	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Panic")
-	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Panic")
-	self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "Panic")
-	self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
-	self:RegisterEvent("BigWigs_RecvSync")
-	self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarPanic", 15)
-	self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarFrenzyStart", 5)
-	self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarFrenzyStop", 5)
-	self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarPanicIni", 10)
+  self:RegisterEvent("CHAT_MSG_MONSTER_EMOTE")
+  self:RegisterEvent("CHAT_MSG_SPELL_AURA_GONE_OTHER")
+  self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE", "Panic")
+  self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE", "Panic")
+  self:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE", "Panic")
+  self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_SELF_DAMAGE", "Panic")
+  self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_PARTY_DAMAGE", "Panic")
+  self:RegisterEvent("CHAT_MSG_SPELL_CREATURE_VS_CREATURE_DAMAGE", "Panic")
+  self:RegisterEvent("PLAYER_REGEN_DISABLED", "CheckForEngage")
+  self:RegisterEvent("BigWigs_RecvSync")
+  self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarPanic", 15)
+  self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarFrenzyStart", 5)
+  self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarFrenzyStop", 5)
+  self:TriggerEvent("BigWigs_ThrottleSync", "MagmadarPanicIni", 10)
 end
 
 ------------------------------
@@ -97,52 +103,47 @@ end
 ------------------------------
 
 function BigWigsMagmadar:CHAT_MSG_MONSTER_EMOTE(msg)
-	if string.find(arg1, L["trigger1"]) and self.db.profile.frenzy then
-		self:TriggerEvent("BigWigs_SendSync", "MagmadarFrenzyStart")
-	end
+  if string.find(arg1, L["trigger1"]) and self.db.profile.frenzy then
+    self:TriggerEvent("BigWigs_SendSync", "MagmadarFrenzyStart")
+  end
 end
 
 function BigWigsMagmadar:CHAT_MSG_SPELL_AURA_GONE_OTHER(msg)
-	if string.find(msg, L["frenzyfade_trigger"]) then
-		self:TriggerEvent("BigWigs_SendSync", "MagmadarFrenzyStop")
-	end
+  if string.find(msg, L["frenzyfade_trigger"]) then
+    self:TriggerEvent("BigWigs_SendSync", "MagmadarFrenzyStop")
+  end
 end
 
 function BigWigsMagmadar:BigWigs_RecvSync(sync, rest, nick)
-	if not self.started and sync == "BossEngaged" and rest == self.bossSync then
-		if firstpanic == 0 then
-			self:TriggerEvent("BigWigs_SendSync", "MagmadarPanicIni")
-		end
-	elseif sync == "MagmadarPanicIni" then
-		firstpanic = 1
-		if self.db.profile.panic then
-			self:ScheduleEvent("BigWigs_Message", 15, L["fearsoon"], "Attention")		
-			self:TriggerEvent("BigWigs_StartBar", self, L["fearbar"], 20, "Interface\\Icons\\Spell_Shadow_DeathScream")
-		end
-        self:TriggerEvent("BigWigs_StartBar", self, "First Frenzy", 29, "Interface\\Icons\\Ability_Druid_ChallangingRoar")
-        --self:TriggerEvent("BigWigs_StartBar", self, "Lava Bomb", 12, "Interface\\Icons\\Spell_Fire_SelfDestruct")
-        --self:ScheduleEvent("BigWigs_SendSync", 12, "MagmadarLavaBomb")
-	elseif sync == "MagmadarPanic" and self.db.profile.panic then
-		self:TriggerEvent("BigWigs_Message", L["feartime"], "Important")
-		self:ScheduleEvent("BigWigs_Message", 30, L["fearsoon"], "Urgent")		
-		self:TriggerEvent("BigWigs_StartBar", self, L["fearbar"], 35, "Interface\\Icons\\Spell_Shadow_DeathScream")
-	elseif sync == "MagmadarFrenzyStart" and self.db.profile.frenzy then
-		self:TriggerEvent("BigWigs_Message", L["frenzyann"], "Important", true, "Alert")
-		self:TriggerEvent("BigWigs_StartBar", self, L["frenzy_bar"], 8, "Interface\\Icons\\Ability_Druid_ChallangingRoar", true, "red")
-        if playerClass == "HUNTER" then
-            self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Spell_Nature_Drowsy", 8, true)
-        end
-	elseif sync == "MagmadarFrenzyStop" and self.db.profile.frenzy then
-        self:TriggerEvent("BigWigs_StopBar", self, L["frenzy_bar"])
-        self:TriggerEvent("BigWigs_HideIcon", "Interface\\Icons\\Spell_Nature_Drowsy")
-    elseif sync == "MagmadarLavaBomb" then
-        --self:TriggerEvent("BigWigs_StartBar", self, "Lava Bomb", 11, "Interface\\Icons\\Spell_Fire_SelfDestruct")
-        --self:ScheduleEvent("BigWigs_SendSync", 11, "MagmadarLavaBomb")
-	end
+  if not self.started and sync == "BossEngaged" and rest == self.bossSync then
+    self:StartFight()
+    self:ScheduleEvent("BigWigs_Message", 5, L["fearsoon"], "Attention")
+    self:TriggerEvent("BigWigs_StartBar", self, L["fear_first_bar"], 10, "Interface\\Icons\\Spell_Shadow_DeathScream")
+    self:TriggerEvent("BigWigs_StartBar", self, L["frenzy_first_bar"], 15, "Interface\\Icons\\Ability_Druid_ChallangingRoar")
+  elseif sync == "MagmadarPanic" and self.db.profile.panic then
+    self:TriggerEvent("BigWigs_StopBar", self, L["fear_first_bar"])
+    self:TriggerEvent("BigWigs_Message", L["feartime"], "Important")
+    self:ScheduleEvent("BigWigs_Message", 25, L["fearsoon"], "Urgent")
+    self:TriggerEvent("BigWigs_StartBar", self, L["fearbar"], 30, "Interface\\Icons\\Spell_Shadow_DeathScream")
+    self:SetCandyBarFade("BigWigsBar "..L["fearbar"], 5)
+  elseif sync == "MagmadarFrenzyStart" and self.db.profile.frenzy then
+    self:TriggerEvent("BigWigs_StopBar", self, L["frenzy_first_bar"])
+    self:TriggerEvent("BigWigs_StopBar", self, L["frenzy_bar"])
+    self:TriggerEvent("BigWigs_Message", L["frenzyann"], "Important", true, "Alert")
+    self:TriggerEvent("BigWigs_StartBar", self, L["frenzy_active_bar"], 8, "Interface\\Icons\\Ability_Druid_ChallangingRoar", true, "red")
+    self:TriggerEvent("BigWigs_StartBar", self, L["frenzy_bar"], 15, "Interface\\Icons\\Ability_Druid_ChallangingRoar")
+    self:SetCandyBarFade("BigWigsBar "..L["frenzy_bar"], 5)
+    if playerClass == "HUNTER" then
+      self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Spell_Nature_Drowsy", 8, true)
+    end
+  elseif sync == "MagmadarFrenzyStop" and self.db.profile.frenzy then
+    self:TriggerEvent("BigWigs_StopBar", self, L["frenzy_active_bar"])
+    self:TriggerEvent("BigWigs_HideIcon", "Interface\\Icons\\Spell_Nature_Drowsy")
+  end
 end
 
 function BigWigsMagmadar:Panic(msg)
-	if ((string.find(msg, L["trigger2"])) or (string.find(msg, L["trigger3"])) or (string.find(msg, L["trigger4"]))) then
-		self:TriggerEvent("BigWigs_SendSync", "MagmadarPanic")
-	end
+  if ((string.find(msg, L["trigger2"])) or (string.find(msg, L["trigger3"])) or (string.find(msg, L["trigger4"]))) then
+    self:TriggerEvent("BigWigs_SendSync", "MagmadarPanic")
+  end
 end
